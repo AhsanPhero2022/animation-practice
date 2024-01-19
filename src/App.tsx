ffa0b455e35572689aeb771f61b466ec4d5b176e
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-01-19 23:04:55 +0600

    rotate skew scale practice done

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,30 @@ function App() {
   return (
     <>
       <main>
-        <div className="box"></div>
-        <h2>hellop</h2>
+        <div className="abir">
+          <div className="main">
+            <div className="box"></div>
+            <h2>Hi</h2>
+          </div>
+          <div className="asia">
+            <div className="box-a"></div>
+            <h2>hello</h2>
+          </div>
+          <div className="asia-2">
+            <div className="box-b"></div>
+            <h2>How are you?</h2>
+          </div>
+        </div>
+        <div className="main-1">
+          <div className="box-1"></div>
+          <div className="container-2">
+            <div className="box-2"></div>
+          </div>
+          <div className="box-3"></div>
+          <div className="box-4"></div>
+          <div className="box-5"></div>
+          <div className="box-6"></div>
+        </div>
       </main>
     </>
   );
