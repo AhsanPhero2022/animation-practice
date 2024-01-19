105d2660b982425b89d18cd144a284b411c492c1
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-01-19 21:50:48 +0600

    simple smooth button working

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="box"></div>
+        <h2>hellop</h2>
+      </main>
+    </>
+  );
+}
+
+export default App;
